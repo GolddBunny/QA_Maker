da2617864aa51d0d9f588dd9008493a2410a5156
SoungJuyeon <tjdwndus1325@gmail.com>
2025-05-24 15:59:10 +0900

    정확도 서버

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,8 @@ from routes.source_routes import source_bp
 from routes.urlSource_routes import url_source_bp
 from routes.parquet import parquet_bp
 from routes.questionGeneration_routes import question_bp
+from routes.accuracy_routes import accuracy_bp
+
 def create_app():
     """Flask 애플리케이션 생성 및 설정"""
     app = Flask(__name__)
@@ -26,6 +28,7 @@ def create_app():
     app.register_blueprint(url_source_bp)
     app.register_blueprint(parquet_bp)
     app.register_blueprint(question_bp)
+    app.register_blueprint(accuracy_bp)
     return app
 
 if __name__ == '__main__':
